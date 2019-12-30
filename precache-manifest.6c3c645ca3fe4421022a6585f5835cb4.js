f428b6287974973f23fea393cf72f18e0ff2a018
Kirankumar Gonti <kirankumar225rk@gmail.com>
2019-12-31 00:01:54 +0530

    Updates

---
+++ precache-manifest.6c3c645ca3fe4421022a6585f5835cb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012b546671a262cf1f59d6f016dea7fb",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "d4d84ec08cec21c7e790",
+    "url": "/React-Todo-List/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2a9f027ceae877787ecc",
+    "url": "/React-Todo-List/static/css/main.d76c462d.chunk.css"
+  },
+  {
+    "revision": "d4d84ec08cec21c7e790",
+    "url": "/React-Todo-List/static/js/2.089c26f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Todo-List/static/js/2.089c26f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a9f027ceae877787ecc",
+    "url": "/React-Todo-List/static/js/main.e018ef30.chunk.js"
+  },
+  {
+    "revision": "76ded148e0d6024e3572",
+    "url": "/React-Todo-List/static/js/runtime-main.1275f770.js"
+  }
+]);
